8b68feb88a31c84f79bf18e1454380cb36715c8a
chatgpt2-crypto <chatgpt2@formetest.abrdns.com>
2026-02-10 13:34:55 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,22 +1,8 @@
-function requireAuth(req, res, next){
-  if(req.session && req.session.user && req.session.user.role === "admin"){
-    return next();
-  }
-  return res.redirect("/login");
+function requireAuth(req,res,next){
+if(!req.session.user){
+return res.redirect("/admin/login");
+}
+next();
 }
 
-function requireCustomer(req, res, next){
-  if(req.session && req.session.customer && req.session.customer.role === "customer"){
-    return next();
-  }
-  return res.redirect("/customer/login");
-}
-
-function requireApiAuth(req, res, next){
-  if(req.session && req.session.user && req.session.user.role === "admin"){
-    return next();
-  }
-  return res.status(401).json({ ok:false, error:"unauthorized" });
-}
-
-module.exports = { requireAuth, requireApiAuth, requireCustomer };
+module.exports = {requireAuth};
